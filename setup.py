df42eee5ab830aa74b93f2daba836f05aeb8796a
Antonio Lopez Rivera <antonlopezr99@gmail.com>
2021-08-05 21:32:05 +0200

    Docs action setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Python-alexandria",
+    name="Python-Alexandria",
     version="2.0",
     author="Antonio Lopez Rivera",
     author_email="antonlopezr99@gmail.com",
